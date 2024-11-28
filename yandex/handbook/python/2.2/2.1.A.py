8a83f5d5360aa88a257b400f9f2785f90a214c4c
Andrei Sharov <andrsharov@gmail.com>
2024-11-29 00:45:38 +0300

    2.2.A

---
+++ yandex/handbook/python/2.2/2.1.A.py
@@ -0,0 +1,9 @@
+print("Как Вас зовут?")
+username = input()
+print(f"Здравствуйте, {username}!")
+print("Как дела?")
+mood = input()
+if mood == "хорошо":
+    print("Я за вас рада!")
+if mood == "плохо":
+    print("Всё наладится!")
